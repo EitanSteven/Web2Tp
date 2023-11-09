1c8b8d569c63dc622be595d46d289cb576b738ba
Eitan Steven Gil <eitansteven2002@hotmail.com>
2023-11-09 18:03:52 -0300

    Subiendo documentacion de los Endpoints

--- routerApi.php
+++ routerApi.php
@@ -11,13 +11,13 @@ $router = new Router();
 
 $router->addRoute("autores", "GET", "apiAutorController", "getAll");
 $router->addRoute("autor/:ID", "GET", "apiAutorController", "get");
-$router->addRoute("autor/:ID", "DELETE", "apiAutorController", "delete");
 $router->addRoute("autores", "POST", "apiAutorController", "add");
 $router->addRoute("autor/:ID", "PUT", "apiAutorController", "update");
+$router->addRoute("autor/:ID", "DELETE", "apiAutorController", "delete");
 
 $router->addRoute("libros", "GET", "apiLibrosController", "getAll");
-$router->addRoute("libros/:ID", "GET", "apiLibrosController", "getAllbyID");
 $router->addRoute("libro/:ID", "GET", "apiLibrosController", "get");
+$router->addRoute("libros/:ID", "GET", "apiLibrosController", "getAllbyID");
 $router->addRoute("libros", "POST", "apiLibrosController", "add");
 $router->addRoute("libro/:ID", "PUT", "apiLibrosController", "update");
 $router->addRoute("libro/:ID", "DELETE", "apiLibrosController", "delete");
